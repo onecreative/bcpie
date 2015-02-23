0324f57573655783ebc7bc9c972fb0eb55a52519
Adam Cook <adam@onecreative.pro>
2015-02-23 14:13:53 -0700

    Update

---
+++ apps/scripts/appHead.js
@@ -0,0 +1,4 @@
+//@codekit-prepend "bower_components/foundation-apps/dist/js/foundation-apps.js";
+//@codekit-prepend "bower_components/foundation/js/vendor/jquery.js";
+//@codekit-prepend "bower_components/foundation/js/vendor/jquery.cookie.js";
+//@codekit-prepend "bcpie.js";
