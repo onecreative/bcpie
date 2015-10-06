c4932a2c5f35deee6868043f28eb761573349b6b
Adam Cook <adam@onecreative.pro>
2015-10-06 10:09:02 -0700

    Created app starter template

---
+++ app starter/scripts/footScripts.js
@@ -0,0 +1,14 @@
+//@codekit-prepend "bower_components/foundation/js/vendor/jquery.js";
+//@codekit-prepend "bower_components/foundation/js/vendor/jquery.cookie.js";
+//@codekit-prepend "bower_components/foundation/js/vendor/fastclick.js";
+//@codekit-prepend "bower_components/foundation/js/vendor/placeholder.js";
+//@codekit-prepend "bower_components/responsive-tables/responsive-tables.js";
+//@codekit-prepend "bower_components/selectize/dist/js/standalone/selectize.js";
+//@codekit-prepend "jquery-ui.min.js";
+//@codekit-prepend "min/iconic.min.js";
+//@codekit-prepend "bcpie.js";
+//@codekit-prepend "AutoSave/autosave.js";
+//@codekit-prepend "Iris/iris.js";
+//@codekit-prepend "Evan/webapp.js";
+//@codekit-prepend "bower_components/foundation/js/foundation.js";
+$(document).foundation();
