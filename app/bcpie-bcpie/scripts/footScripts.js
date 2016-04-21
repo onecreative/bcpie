bac3b4b30282f7f0c62778db1b9b917ca45cfda6
Adam Cook <adam@onecreative.pro>
2016-04-20 23:49:14 -0600

    Templates are now created automatically

--- app/bcpie-bcpie/scripts/footScripts.js
+++
@@ -1,9 +0,0 @@
-//@codekit-prepend "bower_components/jquery/dist/jquery.js";
-//@codekit-prepend "bower_components/what-input/what-input.js";
-//@codekit-prepend "bower_components/foundation-sites/dist/foundation.js";
-//@codekit-prepend "bower_components/motion-ui/dist/motion-ui.js";
-//codekit-prepend "iconic-everything-1.9.0/js/iconic.min.js";
-//@codekit-prepend "bcpie.js";
-//@codekit-prepend "uglify/uglifyjs.js";
-//@codekit-prepend "bower_components/alertify-js/build/alertify.min.js";
-$(document).foundation();
